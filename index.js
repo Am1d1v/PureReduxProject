62d800f72933123ad3cf5f56445d025206e4fb7f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-03 18:31:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+import store from "./redux/store.js";
+
+//console.log(store.getState());
+
+const unsubscribe =  store.subscribe(() => {
+    console.log(`Redux store just changed ${store.getState()}`);
+})
+
+store.dispatch({
+    type: 'Add_Current_Time',
+    payload: '15: 00'
+});
+//console.log(store.getState());
 
 
+store.dispatch({
+    type: 'Add_Current_Time',
+    payload: '16: 00'
+});
+//console.log(store.getState());
 
+unsubscribe();
+
+store.dispatch({
+    type: 'Clear_All_Times'
+})
+//console.log(store.getState());
